30ccfd4551ad8bc1375090fb90ecb83ca4091861
bankya <bankiadebajo@hotmail.com>
2018-03-18 14:23:40 -0400

    Add motor control functionality

--- app/motor_driver/src/main.cpp
+++ app/motor_driver/src/main.cpp
@@ -1,7 +1,7 @@
 /**
   ******************************************************************************
   * @file    main.c
-  * @author  Banky
+  * @author  Alice, Banky
   * @version V1.0
   * @date    01-Februrary-2018
   * @brief   Default main function.
@@ -22,32 +22,29 @@ extern "C" {
 Serial pc(USBTX, USBRX);
 uint32_t pwm_ID = 400;
 float freq = 50;
-PwmOut led(D5);
-
-int main()
-{
-    if (CANLIB_Init(pwm_ID, CANLIB_LOOPBACK_ON) != 0)
-    {
-        pc.printf("CAN Initialization Failed\r\n");
-    }
-    if (CANLIB_AddFilter(500) != 0)
-    {
-        pc.printf("CAN Add Filter Failed\r\n");
-    }
-}
+PwmOut pwm_pin(PB_15);
+DigitalOut dir_pin(PC_6);
 
 /*
  *  Generates a PWM signal with a duty cycle of pwm_duty/256
  */
-
 void pwm_gen(uint32_t pwm_duty)
 {
-    float period = 1/freq;	
-    led.period(period);
+    float period = 1/freq;  
+    pwm_pin.period(period);
     if (pwm_duty <= 255)
     {
-        led.write((float)pwm_duty/255.0);
+        pwm_pin.write((float)pwm_duty/255.0);
     }
+}
+
+/*
+ *  Controls the motor speed. Direction = 1 is clockwise
+ */
+void motor_control(int16_t speed)
+{
+    pwm_gen(abs(speed));
+    dir_pin = (speed <= 0);
 }
 
 /*
@@ -57,6 +54,36 @@ void CANLIB_Rx_OnMessageReceived(void)
 {
     uint32_t pwm_duty= CANLIB_Rx_GetAsUint(CANLIB_INDEX_0);
     pwm_gen(pwm_duty);
+}
+
+int main()
+{
+    if (CANLIB_Init(pwm_ID, CANLIB_LOOPBACK_ON) != 0)
+    {
+        pc.printf("CAN Initialization Faipwm_pin\r\n");
+    }
+    if (CANLIB_AddFilter(500) != 0)
+    {
+        pc.printf("CAN Add Filter Faipwm_pin\r\n");
+    }
+
+    // Test motor by moving it forward and backwards
+    int16_t speed = 0;
+    int16_t increment = 10;
+    int16_t max_speed = 255;
+
+    while (true) {
+        pwm_gen(speed);
+        speed += increment;
+        if ((speed < -max_speed && increment < 0) || (speed > max_speed && increment > 0))
+        {
+            increment = -increment;
+        }
+
+        motor_control(speed);
+        wait(0.2);
+    }
+
 }
 #ifdef __cplusplus
 }
